ce7d2b33922fd8c71dc72eb5d168294f0f7f1046
LMO GerritBot <gerrit@libremobileos.com>
2024-04-19 19:23:14 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_channel.mk
+    $(LOCAL_DIR)/lmodroid_channel.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_channel-eng \
-    lineage_channel-user \
-    lineage_channel-userdebug
+    lmodroid_channel-eng \
+    lmodroid_channel-user \
+    lmodroid_channel-userdebug
--- lmodroid_channel.mk
+++ lmodroid_channel.mk
@@ -1,6 +1,6 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Device
@@ -11,7 +11,7 @@ PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := channel
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := moto g(7) play
-PRODUCT_NAME := lineage_channel
+PRODUCT_NAME := lmodroid_channel
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=channel \
